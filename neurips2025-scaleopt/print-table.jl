77f1b3f7ae95881c93a4addc80a86f68077e674e
Robert Parker <robbybparker@gmail.com>
2026-03-06 12:03:35 -0500

    remove unnecessary code

--- neurips2025-scaleopt/print-table.jl
+++
@@ -1,9 +0,0 @@
-import DataFrames
-import CSV
-if length(ARGS) < 1
-    error("Provide a CSV file as the first argument")
-elseif length(ARGS) > 1
-    @warn "Additional arguments (beyond $(ARGS[1])) are ignored"
-end
-df = DataFrames.DataFrame(CSV.File(ARGS[1]))
-println(df)
