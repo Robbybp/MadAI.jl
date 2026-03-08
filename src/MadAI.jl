a9389f2cf60e21e6bf0f266cb48b9dbfd8ec533f
Robert Parker <robbybparker@gmail.com>
2026-03-08 12:58:30 -0400

    add MadAI.jl

---
+++ src/MadAI.jl
@@ -0,0 +1,22 @@
+module MadAI
+
+import SparseArrays
+
+import JuMP
+import NLPModels
+import NLPModelsJuMP
+import HSL
+import MadNLP
+import MadNLPHSL
+import MathOptAI
+import MathProgIncidence
+
+include("linalg.jl")
+include("blockdiagonal.jl")
+include("btsolver.jl")
+include("formulation.jl")
+include("nlpmodels.jl")
+include("kkt-partition.jl")
+include("ma48.jl")
+
+end
