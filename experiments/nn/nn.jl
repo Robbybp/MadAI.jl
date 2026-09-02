4e4e216e7ab373799c0dc2fa9f9f49fe0a9fb86b
Robert Parker <robbybparker@gmail.com>
2026-09-02 10:54:28 -0400

    SCOPF architecture and weights

--- experiments/nn/nn.jl
+++ experiments/nn/nn.jl
@@ -2,12 +2,15 @@ import PythonCall
 FILEDIR = @__DIR__
 PythonCall.pyimport("sys").path.append(FILEDIR)
 pymnist = PythonCall.pyimport("train_mnist")
+pyscopf = PythonCall.pyimport("scopf")
 
 function get_nn(modelname, nodes, layers)
     if lowercase(modelname) == "mnist"
         # Note that this function returns an inference-ready network with Softmax
         # and weights loaded.
-        return pymnist.create_nn_architecture(nodes, layers, "tanh")
+        return pymnist.create_nn_architecture(nodes, layers, "tanh"; load_weights = true)
+    elseif lowercase(modelname) == "scopf"
+        return pyscopf.create_nn_architecture(nodes, layers, "tanh"; load_weights = true)
     else
         error("Unexpected model name: $modelname")
     end
