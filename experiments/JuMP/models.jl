70048318b9448c8bd520932905ebc61767126d64
Robert Parker <robbybparker@gmail.com>
2026-09-07 12:00:16 -0400

    add LSV

--- experiments/JuMP/models.jl
+++ experiments/JuMP/models.jl
@@ -9,6 +9,10 @@ module SCOPF
 include("scopf/scopf.jl")
 end
 
+module LSV
+include("lsv/lsv.jl")
+end
+
 # This is only necessary for the method where we construct straight from
 # name, nodes, and layers.
 include("../nn/nn.jl")
@@ -23,6 +27,19 @@ function get_model(modelname, nnfile; kwds...)
         return m, formulation
     elseif lowercase(modelname) == "scopf"
         m, formulation = SCOPF.get_scopf_model(nnfile; kwds...)
+        pivot_vars, _ = MadAI.get_vars_cons(formulation)
+        for var in pivot_vars
+            # Pivot vars should not be fixed (they must exist in the KKT system),
+            # so, if they are, we relax the bounds.
+            if JuMP.has_lower_bound(var) && JuMP.has_upper_bound(var) && JuMP.lower_bound(var) == JuMP.upper_bound(var)
+                JuMP.set_lower_bound(var, JuMP.lower_bound(var) - 1e-6)
+                JuMP.set_upper_bound(var, JuMP.upper_bound(var) + 1e-6)
+            end
+        end
+        return m, formulation
+    elseif lowercase(modelname) == "lsv"
+        # NN already includes normalization as a first input layer
+        m, formulation = LSV.get_maxls_model(nnfile; normalize = false, kwds...)
         pivot_vars, _ = MadAI.get_vars_cons(formulation)
         for var in pivot_vars
             # Pivot vars should not be fixed (they must exist in the KKT system),
