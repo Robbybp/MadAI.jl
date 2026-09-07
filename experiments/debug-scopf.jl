0c0d0451caae8cda269f242e7ef3d070d1ff1896
Robert Parker <robbybparker@gmail.com>
2026-09-07 12:01:50 -0400

    update debugging script

--- experiments/debug-scopf.jl
+++ experiments/debug-scopf.jl
@@ -12,27 +12,33 @@ layers = 5
 
 model, formulation = get_model(modelname, nodes, layers)
 
-pivot_vars, pivot_cons = MadAI.get_vars_cons(formulation)
-for var in pivot_vars
+for var in JuMP.all_variables(model)
     if JuMP.has_lower_bound(var) && JuMP.has_upper_bound(var) && JuMP.lower_bound(var) == JuMP.upper_bound(var)
-        JuMP.delete(model, JuMP.LowerBoundRef(var))
-        JuMP.delete(model, JuMP.UpperBoundRef(var))
+        println("WARNING: Variable $var is fixed to $(JuMP.upper_bound(var))")
     end
 end
 
-nlp = NLPModelsJuMP.MathOptNLPModel(model)
-madnlp = MadNLP.MadNLPSolver(nlp; fixed_variable_treatment=MadNLP.RelaxBound)
-MadNLP.initialize!(madnlp)
-matrix = MadNLP.get_kkt(MadNLP.get_kkt(madnlp))
-
-pivot_indices = convert(Vector{Int32}, MadAI.get_kkt_indices(model, pivot_vars, pivot_cons))
-blocks = MadAI.partition_indices_by_layer(model, formulation; indices = pivot_indices)
-pivot_solver_opt = MadAI.BlockTriangularOptions(; blocks)
-opt = MadAI.SchurComplementOptions(;
-    ReducedSolver = MadNLPHSL.Ma57Solver,
-    PivotSolver = MadAI.BlockTriangularSolver,
-    pivot_indices,
-    pivot_solver_opt,
-)
-
-linear_solver = MadAI.SchurComplementSolver(matrix; opt)
+#pivot_vars, pivot_cons = MadAI.get_vars_cons(formulation)
+#for var in pivot_vars
+#    if JuMP.has_lower_bound(var) && JuMP.has_upper_bound(var) && JuMP.lower_bound(var) == JuMP.upper_bound(var)
+#        #JuMP.delete(model, JuMP.LowerBoundRef(var))
+#        #JuMP.delete(model, JuMP.UpperBoundRef(var))
+#    end
+#end
+#
+#nlp = NLPModelsJuMP.MathOptNLPModel(model)
+#madnlp = MadNLP.MadNLPSolver(nlp; fixed_variable_treatment=MadNLP.RelaxBound)
+#MadNLP.initialize!(madnlp)
+#matrix = MadNLP.get_kkt(MadNLP.get_kkt(madnlp))
+#
+#pivot_indices = convert(Vector{Int32}, MadAI.get_kkt_indices(model, pivot_vars, pivot_cons))
+#blocks = MadAI.partition_indices_by_layer(model, formulation; indices = pivot_indices)
+#pivot_solver_opt = MadAI.BlockTriangularOptions(; blocks)
+#opt = MadAI.SchurComplementOptions(;
+#    ReducedSolver = MadNLPHSL.Ma57Solver,
+#    PivotSolver = MadAI.BlockTriangularSolver,
+#    pivot_indices,
+#    pivot_solver_opt,
+#)
+#
+#linear_solver = MadAI.SchurComplementSolver(matrix; opt)
